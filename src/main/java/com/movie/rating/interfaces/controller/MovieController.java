f55cd43c4e3898d722fa546865b46e448a9b583a
Ying Zhou <ybzhou@thoughtworks.com>
2024-08-11 18:13:57 +0800

    feat: generate api doc with OpenApi starter

--- src/main/java/com/movie/rating/interfaces/controller/MovieController.java
+++ src/main/java/com/movie/rating/interfaces/controller/MovieController.java
@@ -4,6 +4,11 @@ import com.movie.rating.appservice.MovieAppService;
 import com.movie.rating.domain.Movie;
 import com.movie.rating.interfaces.controller.representation.MovieRepresentation;
 import com.movie.rating.interfaces.controller.representation.PageRepresentation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +29,38 @@ public class MovieController {
         this.movieAppService = movieAppService;
     }
 
+    @Operation(summary = "List movies", description = "Retrieve a list of movies",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successful operation",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = PageRepresentation.class),
+                                    examples = @ExampleObject(value = """
+                                                    {
+                                                        "content": [
+                                                            {
+                                                                "id": 1,
+                                                                "title": "Inception",
+                                                                "runtime": 148,
+                                                                "director": "Christopher Nolan",
+                                                                "actors": "Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page",
+                                                                "plot": "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
+                                                                "posterUrl": "https://example.com/inception.jpg"
+                                                            },
+                                                            {
+                                                                "id": 2,
+                                                                "title": "The Matrix",
+                                                                "runtime": 136,
+                                                                "director": "Lana Wachowski, Lilly Wachowski",
+                                                                "actors": "Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss",
+                                                                "plot": "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
+                                                                "posterUrl": "https://example.com/matrix.jpg"
+                                                            }
+                                                        ],
+                                                        "totalSize": 2
+                                                    }
+                                            """)))
+            }
+    )
     @GetMapping
     public ResponseEntity<PageRepresentation<MovieRepresentation>> listMovies(
             @RequestParam(value = "pageNumber", required = false, defaultValue = "1") Integer pageNumber,
